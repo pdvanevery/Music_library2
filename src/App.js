e3effe84502675bde34288e2edc7f6ece17ef823
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-04-22 12:54:36 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+
+function App(){
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
+
+    useEffect(() => {
+      const fetchData = async () => {
+          document.title = `${search} Music`
+          const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+          const resData = await response.json()
+          if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
+      }
+      fetchData()
+  }, [search])
+  
+    return (
+        <div>
+            <SearchBar />
+            {message}
+            <Gallery />
+        </div>
+    )
+}
+
+export default App
+
+
