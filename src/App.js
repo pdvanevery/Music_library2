da0d00021a5d1bac1c85c80079385f9dacac1127
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-04-22 18:42:00 -0700

    updated it with context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
+
 
 
 const App = () => {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+  let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
@@ -32,10 +36,17 @@ const App = () => {
   }
 
   return (
-      <div>
-          <SearchBar handleSearch={handleSearch} />
+      <div className='App'>
+          <SearchContext.Provider value={{
+              term: searchInput,
+              handleSearch: handleSearch
+          }}>
+              <SearchBar/>
+          </SearchContext.Provider>
           {message}
-          <Gallery data={data} />
+          <DataContext.Provider value={data}>
+          <Gallery />
+          </DataContext.Provider>
       </div>
   )
 }
